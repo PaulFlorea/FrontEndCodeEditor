57cce0a406a682d4c1b7b06215597cc0c8fb1eb5
PaulFlorea <paul.florea6@gmail.com>
2015-03-30 01:02:36 -0400

    Some code cleanup

--- js/angular/app.js
+++ js/angular/app.js
@@ -1,7 +1,8 @@
-app = angular.module('myApp', []);
+app = angular.module('editorApp', []);
 
 app.controller("MainController", ['$scope', '$sce', function($scope, $sce) {
 
+	//Modifiable models
 	$scope.html = '<body>'+
 	'\n\t<h1>Hello World</h1>'+
 	'\n</body>';
@@ -13,6 +14,8 @@ app.controller("MainController", ['$scope', '$sce', function($scope, $sce) {
 	'\n\tmargin:60px 0px;'+
 	'\n}';
 
+
+	//Used for wrapping the html and css
 	$scope.head_s = '<!DOCTYPE HTML>'+
 	'\n<html>'+
 	'\n\t<head>'+
@@ -22,9 +25,11 @@ app.controller("MainController", ['$scope', '$sce', function($scope, $sce) {
 	'\n\t</head>';
 	
 
+	//Resultant model
 	$scope.myHTML = $scope.head_s +  $scope.css + $scope.head_e + $scope.html + '\n</html>';
 	$scope.myHTMLSafe = $sce.trustAsHtml($scope.myHTML);
 
+	//Updates the result with every change
 	$scope.update = function(){
 		$scope.myHTML = $scope.head_s +  $scope.css + $scope.head_e + $scope.html + '\n</html>';
 		$scope.myHTMLSafe = $sce.trustAsHtml($scope.myHTML);
